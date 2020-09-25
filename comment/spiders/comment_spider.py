6d2b54b9a6228dac33eae3ba5aea510ddbfe5ba7
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-09-25 13:48:57 +0330

    digikala crawler bug!

--- comment/spiders/comment_spider.py
+++ comment/spiders/comment_spider.py
@@ -9,22 +9,14 @@ class CommentSpider(scrapy.Spider):
         self.base_url = "https://www.digikala.com/ajax/product/comments/{}/?mode=newest_comment&page={}"
         self.title = "//h2[@class='c-comments__headline']/span/span/text()"
         self.container = "//div[@id='product-comment-list']/ul[@class='c-comments__list']/li/section/div[@class='article']"
-        self.header = self.container + "/div[@class='header']/div/text()"
-        self.comment_text = self.container + "/p/text()"
-        self.positive = self.container + "/div[@class='c-comments__evaluation']/div[@class='c-comments__evaluation-positive']/ul/li/text()"
-        self.negative = self.container + "/div[@class='c-comments__evaluation']/div[@class='c-comments__evaluation-negative']/ul/li/text()"
+        self.header = "//div[@class='header']/div/text()"
+        self.comment_text = "/p/text()"
+        self.positive = "//div[@class='c-comments__evaluation']/div[@class='c-comments__evaluation-positive']/ul/li/text()"
+        self.negative = "//div[@class='c-comments__evaluation']/div[@class='c-comments__evaluation-negative']/ul/li/text()"
         self.pages = "//div[@class='c-pager']"
         self.all_pages = self.pages + "/ul/li/a/@href"
-        self.product_no = 3440000
-        # self.product_no = 20
-        self.df = pd.DataFrame(
-            columns=[
-                "product_id",
-                "comment",
-                "positive",
-                "negative",
-            ]
-        )
+        # self.product_no = 3440000
+        self.product_no = 200
         self.all_urls = set(self.base_url.format(i, 1) for i in range(2, self.product_no))
 
     def url_parser(self, url):
@@ -45,17 +37,24 @@ class CommentSpider(scrapy.Spider):
         if response.status == 200:
             title = response.xpath(self.title).get()
             if title:
-                header = response.xpath(self.header).get()
-                comments = response.xpath(self.comment_text).getall()
-                positive = response.xpath(self.positive).getall()
-                negative = response.xpath(self.negative).getall()
-                all_pages = response.xpath(self.all_pages).getall()
-                yield {
-                    'product_id': product_id,
-                    'header': header,
-                    'title': title,
-                    'comments': comments
-                }
+                comments = response.xpath(self.container)
+                for i, comment in enumerate(comments):
+                    header = comment.xpath(self.header).get()
+                    comment_text = comment.xpath(self.comment_text).get()
+                    positive = comment.xpath(self.positive).get()
+                    negative = comment.xpath(self.negative).get()
+
+                    yield {
+                        'product_id': product_id,
+                        'title': title,
+                        'header': header,
+                        'title': title,
+                        'comment': comment_text,
+                        'positive': positive,
+                        'negative': negative,
+                    }
+                
+                all_pages = response.xpath(self.all_pages).get()
                 if all_pages:
                     last_page = int(all_pages[-1].split("=")[-1])
                     if (product_id, last_page) not in self.crawled:
@@ -66,3 +65,5 @@ class CommentSpider(scrapy.Spider):
                     
             else:
                 return
+        else:
+            return
